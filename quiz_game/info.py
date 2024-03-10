5d5a381abbe27ccc1197bf391f30ee30cf98159d
Abd el Madjid Kahoul <105085145+Abd-el-madjid@users.noreply.github.com>
2024-03-11 00:12:59 +0100

    Update info.py

--- quiz_game/info.py
+++ quiz_game/info.py
@@ -1,7 +1,7 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'abdelmadjid.kahoul@univ-constantine2.dz'
-EMAIL_HOST_PASSWORD = 'Madjid1204!@)$'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 
