eff71479ed9857f5bf36ea686429b07ff5af210e
etudiant3 <etudiant3@gmail.com>
2024-03-12 18:33:32 +0100

    organize final

--- quiz_game/urls.py
+++ quiz_game/urls.py
@@ -1,6 +1,10 @@
 from django.urls import include, path
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,3 +19,7 @@ urlpatterns = [
     path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"),
          name='password_reset_complete'),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
